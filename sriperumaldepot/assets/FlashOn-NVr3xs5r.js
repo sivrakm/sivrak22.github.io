98cc6f4127a81b3ddff10adc0e60a383a858667a
sivrak22 <sivrak.m@gmail.com>
2026-01-06 23:22:12 +0530

    update build

---
+++ sriperumaldepot/assets/FlashOn-NVr3xs5r.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-CA5h76Mg.js";import{c as s}from"./Button-C82hmiKU.js";const a=s(o.jsx("path",{d:"M7 2v11h3v9l7-12h-4l4-8z"}));export{a as F};
---
+++ sriperumaldepot/assets/signout-DUI25DHH.js
@@ -0,0 +1,1 @@
+import{u,a,r as n}from"./index-CA5h76Mg.js";function r(){const t=u(e=>e.logout),o=a();return n.useEffect(()=>{t(),o({to:"/"})},[t,o]),null}export{r as component};
