f58682dd56b948825f1243f706547e4f949fae65
sivrak22 <sivrak.m@gmail.com>
2026-01-07 22:58:28 +0530

    update build

---
+++ sriperumaldepot/assets/Add-w2aQt3U8.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-BcxSdZbh.js";import{c as t}from"./Button-D7ynFjlK.js";const h=t(o.jsx("path",{d:"M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6z"}));export{h as A};
---
+++ sriperumaldepot/assets/FlashOn-DWXBSP_B.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-BcxSdZbh.js";import{c as s}from"./Button-D7ynFjlK.js";const a=s(o.jsx("path",{d:"M7 2v11h3v9l7-12h-4l4-8z"}));export{a as F};
