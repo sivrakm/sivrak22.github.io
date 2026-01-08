ae6f16bfd1352007f850755d8b82036d74c8ece5
sivrak22 <sivrak.m@gmail.com>
2026-01-08 09:55:30 +0530

    update build

---
+++ sriperumaldepot/assets/CheckCircle-DsJ6hE7C.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-DbYby3O1.js";import{c as r}from"./Button-8gQe2aB5.js";const c=r(o.jsx("path",{d:"M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2m-2 15-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8z"}));export{c as C};
---
+++ sriperumaldepot/assets/orderStore-DYKH7IJx.js
@@ -0,0 +1,1 @@
+import{c as t}from"./index-DbYby3O1.js";const s=t(r=>({lastOrder:null,setLastOrder:e=>r({lastOrder:e}),clearLastOrder:()=>r({lastOrder:null})}));export{s as u};
