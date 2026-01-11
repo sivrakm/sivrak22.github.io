d8f83dccb36b9e74bd28827991059d2d1e1415f7
sivrak22 <sivrak.m@gmail.com>
2026-01-11 11:11:34 +0530

    update build

---
+++ sriperumaldepot/assets/FlashOn-DaAbYSru.js
@@ -0,0 +1,1 @@
+import{f as s,j as o}from"./index-CVOYqxVk.js";const a=s(o.jsx("path",{d:"M7 2v11h3v9l7-12h-4l4-8z"}));export{a as F};
---
+++ sriperumaldepot/assets/orderStore-O7gDCRgV.js
@@ -0,0 +1,1 @@
+import{m as t}from"./index-CVOYqxVk.js";const s=t(r=>({lastOrder:null,setLastOrder:e=>r({lastOrder:e}),clearLastOrder:()=>r({lastOrder:null})}));export{s as u};
